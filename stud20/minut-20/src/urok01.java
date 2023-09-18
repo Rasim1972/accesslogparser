6291be8e8776ad337524a9b8271d995ac96c0747
Mullahmetov Rasim <rrmullakhmetov@inno.tech>
2023-09-18 13:01:43 +0300

    для 1-го разработчика

---
+++ stud20/minut-20/src/urok01.java
@@ -0,0 +1,8 @@
+public class urok01 {
+    public static void main(String[] args) {
+        System.out.println("help1");
+        System.out.println("help2");
+        System.out.println("help3");
+
+    }
+}
